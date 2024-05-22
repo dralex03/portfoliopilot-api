7de10e49ffc385dd92c2bf39651690bcc985c78c
Johannes Siegling <johannes.siegling@eviden.com>
2024-05-22 16:33:56 +0200

    Fix: added small comment

--- src/config.py
+++ src/config.py
@@ -12,4 +12,4 @@ JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
 
 # Other setting variables
 MINIMUM_PASSWORD_LEN = 8
-JWT_EXPIRY = datetime.timedelta(days=0, minutes=15)
+JWT_EXPIRY = datetime.timedelta(days=0, minutes=15) # 15 minute session validity
