c2d3106ae9c0273f8de53e14a763504bc7e9b73f
Maxi580 <maxieichenseher@gmail.com>
2024-05-30 17:10:12 +0200

    Fix: Fixed and improved Tests

--- tests/database_test.py
+++ tests/database_test.py
@@ -2,7 +2,7 @@ import logging
 from src.database.connection import database
 from src.database.models import Portfolio, User, Asset, AssetType, PortfolioElement
 from src.database.queries import insert_new_user, add_portfolio, insert_portfolio_element, remove_portfolio_element, \
-                                 get_user_by_email, delete_portfolio_by_id, reduce_portfolio_element
+    get_user_by_email, delete_portfolio_by_id, reduce_portfolio_element
 
 # Set up logging
 logging.basicConfig(level=logging.DEBUG)
@@ -26,83 +26,88 @@ setup_database()
 
 
 def test_user_insertion():
-    logger.info('Starting test_user_insertion')
     user = insert_new_user(user_email, 'password')
     assert user is not None
     assert user.email == user_email
-    logger.debug(f'Inserted user: {user}')
 
     fetched_user = database.session.query(User).filter_by(email=user_email).first()
-    logger.debug(f'Fetched user: {fetched_user}')
     assert fetched_user is not None
 
 
 def test_get_user_by_email():
-    logger.info('Starting to find test_user')
     user = get_user_by_email(user_email)
     assert user is not None
     assert user.email == user_email
-    logger.debug(f'Found user: {user}')
 
 
 def test_portfolio_insertion():
-    logger.info('Starting test_portfolio_insertion')
-    portfolio = add_portfolio('Welt portfolio', 1)
-    assert portfolio is True
-    logger.debug(f'Portfolio was inserted: {portfolio}')
+    PORTFOLIO_NAME = 'Welt portfolio'
+    portfolio = add_portfolio(PORTFOLIO_NAME, 1)
+    assert portfolio.name == PORTFOLIO_NAME
+    assert portfolio.user_id == 1
 
-    fetched_portfolio = database.session.query(Portfolio).filter_by(name='Welt portfolio', user_id=1).first()
-    logger.debug(f'Fetched portfolio: {fetched_portfolio}')
+    fetched_portfolio = database.session.query(Portfolio).filter_by(name=PORTFOLIO_NAME, user_id=1).first()
     assert fetched_portfolio is not None
-    assert fetched_portfolio.name == 'Welt portfolio'
+    assert fetched_portfolio.name == PORTFOLIO_NAME
     assert fetched_portfolio.user_id == 1
 
 
 def test_portfolio_element_insertion():
-    logger.info('Starting test_portfolio_element_insertion')
     portfolio_element = insert_portfolio_element(1, 1, 10.0, 10.0,
                                                  10.0)
-    assert portfolio_element is True
+    assert portfolio_element.portfolio_id == 1
+    assert portfolio_element.asset_id == 1
+    assert portfolio_element.order_fee == 10.0
+    assert portfolio_element.buy_price == 10.0
+    assert portfolio_element.order_fee == 10.0
 
     fetched_portfolio_element = database.session.query(PortfolioElement).filter_by(portfolio_id=1, asset_id=1).first()
-    logger.debug(f'Fetched portfolio: {fetched_portfolio_element}')
     assert fetched_portfolio_element is not None
     assert fetched_portfolio_element.portfolio_id == 1
     assert fetched_portfolio_element.asset_id == 1
 
 
 def test_portfolio_element_removal():
-    logger.info('Starting test_portfolio_element removal')
-    portfolio_element = reduce_portfolio_element(1, 1, 5.00)
-    assert portfolio_element is True
+    portfolio_element = reduce_portfolio_element(1, 1, 5.0)
+    assert portfolio_element.portfolio_id == 1
+    assert portfolio_element.asset_id == 1
+    assert portfolio_element.order_fee == 5.0
 
     fetched_portfolio_element = database.session.query(PortfolioElement).filter_by(portfolio_id=1, asset_id=1).first()
-    logger.debug(f'Fetched portfolio_element: {fetched_portfolio_element}')
     assert fetched_portfolio_element is not None
-    assert fetched_portfolio_element.count == 5.00
+    assert fetched_portfolio_element.count == 5.0
     assert fetched_portfolio_element.portfolio_id == 1
     assert fetched_portfolio_element.asset_id == 1
 
 
-def test_portfolio_deletion():
-    logger.info('Starting test_portfolio_deletion')
-    portfolio = delete_portfolio_by_id(1)
-    assert portfolio is True
-
-    fetched_portfolio = database.session.query(Portfolio).filter_by(id=1).first()
-    logger.debug(f'Search Results for Portfolio after Deletion: {fetched_portfolio}')
-    assert fetched_portfolio is None
+def test_portfolio_element_deletion():
+    portfolio_element_deletion = delete_portfolio_by_id(1)
+    assert portfolio_element_deletion is True
 
     fetched_portfolio_element = database.session.query(PortfolioElement).filter_by(portfolio_id=1, asset_id=1).first()
-    logger.debug(f'Search Results for Portfolio_Element after Deletion: {fetched_portfolio_element}')
     assert fetched_portfolio_element is None
 
+
+def test_portfolio_deletion():
+    portfolio_deletion = delete_portfolio_by_id(1)
+    assert portfolio_deletion is True
+
+    fetched_portfolio = database.session.query(Portfolio).filter_by(id=1).first()
+    assert fetched_portfolio is None
+
+
+def test_consequences_of_portfolio_deletion():
+    portfolio = add_portfolio("Test Portfolio", 1)
+    id_to_remove = portfolio.id
+
+    insert_portfolio_element(id_to_remove, 1, 10.0, 10.0, 10.00)
+    delete_portfolio_by_id(id_to_remove)
+
+    fetched_portfolio = database.session.query(Portfolio).filter_by(portfolio_id=id_to_remove).first()
+    assert fetched_portfolio is None
+
     fetched_asset = database.session.query(Asset).filter_by(id=1).first()
-    logger.debug(f'Search Results for Asset after Deletion: {fetched_asset}')
     assert fetched_asset is not None
 
     fetched_asset_type = database.session.query(Asset).filter_by(id=1).first()
-    logger.debug(f'Search Results for Asset_type after Deletion: {fetched_asset_type}')
     assert fetched_asset_type is not None
-
-
