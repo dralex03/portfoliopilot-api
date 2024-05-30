6eb5fa36718f72cea9aa35e3078a2c4829cdb208
Maxi580 <maxieichenseher@gmail.com>
2024-05-30 17:18:38 +0200

    Fix: Fixed an Error where the wrong attribute has been used

--- tests/database_test.py
+++ tests/database_test.py
@@ -2,7 +2,7 @@ import logging
 from src.database.connection import database
 from src.database.models import Portfolio, User, Asset, AssetType, PortfolioElement
 from src.database.queries import insert_new_user, add_portfolio, insert_portfolio_element, \
-                                get_user_by_email, delete_portfolio_by_id, reduce_portfolio_element
+    get_user_by_email, delete_portfolio_by_id, reduce_portfolio_element
 
 # Set up logging
 logging.basicConfig(level=logging.DEBUG)
@@ -51,7 +51,7 @@ def test_portfolio_insertion():
 
 def test_portfolio_element_insertion():
     insert_portfolio_element(1, 1, 10.0, 10.0,
-                                                 10.0)
+                             10.0)
 
     fetched_portfolio_element = database.session.query(PortfolioElement).filter_by(portfolio_id=1, asset_id=1).first()
     assert fetched_portfolio_element is not None
@@ -95,7 +95,7 @@ def test_consequences_of_portfolio_deletion():
     insert_portfolio_element(id_to_remove, 1, 10.0, 10.0, 10.00)
     delete_portfolio_by_id(id_to_remove)
 
-    fetched_portfolio = database.session.query(Portfolio).filter_by(portfolio_id=id_to_remove).first()
+    fetched_portfolio = database.session.query(Portfolio).filter_by(id=id_to_remove).first()
     assert fetched_portfolio is None
 
     fetched_asset = database.session.query(Asset).filter_by(id=1).first()
