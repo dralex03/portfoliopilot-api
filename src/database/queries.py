8ec1dfe1b9e9793324d9f30305b29c35e14a3d8d
Johannes Siegling <johannes.siegling@eviden.com>
2024-06-02 21:41:04 +0200

    Feature: added new database query and update function comments and -heads

--- src/database/queries.py
+++ src/database/queries.py
@@ -1,25 +1,36 @@
 from src.database import models
 from src.database.connection import session
 
-def get_user_by_email(email):
+def get_user_by_email(email: str):
     """
     Fetches a user by email from the database
         Parameters:
-            email: str
+            str email;
         Returns:
             User: user object
     """
 
-    return session.query(models.User).filter(models.User.email == email).first()
+    return session.query(models.User).filter(models.User.email == email).one()
 
-def insert_new_user(email, password):
+def get_user_by_id(id: int):
+    """
+    Fetches a user by ID from the database
+        Parameters:
+            int id;
+        Returns:
+            User: user object
+    """
+
+    return session.query(models.User).filter(models.User.id == id).one()
+
+def insert_new_user(email: str, password: str):
     """
     Inserts new user into database and returns the created object
         Parameters:
-            email: str
-            password: str
+            str email;
+            str password;
         Returns:
-            User: created user object
+            User: newly created user object
     """
 
     try:
